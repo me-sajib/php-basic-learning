f6192bae02d73460cff61a5d4031615abe5ec5fe
sajib <me.mrsajib@gmail.com>
2022-07-09 20:14:06 +0600

    datatype check

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
 
+// datatype check
+// string
+$str = 'Hello World';
+// number
+$num = 123;
+// boolean  
+$bool = true;
+// array
+$arr = [1, 2, 3];
+// object
+$obj = new stdClass();
+// null
+$null = null;
+// resource
+$res = fopen('index.php', 'r');
+// undefined
+$undef = $undef;
+
     // decare variables
     $name = "sajib sarker";
     $age = "20";
