93369c9d90095c3748e04c104f3ff35f3f0c912a
sajib <me.mrsajib@gmail.com>
2022-07-24 11:50:05 +0600

    learn abstract classes

--- oop.php
+++ oop.php
@@ -73,3 +73,41 @@ class bye{
 
 $goodbye = new bye();
 $goodbye->bye();
+
+// abstract classes
+
+// abstract class Car{
+//     public $name;
+//     public $color;
+
+//     public function __construct($name, $color){
+//         $this->name = $name;
+//         $this->color = $color;
+//     }   
+//     abstract public function intro():void;
+// }
+// // child classes
+// class Volvo extends Car{
+//     public function intro():void{
+//         echo "Car ".$this->name." is ".$this->color."<br>";
+//     }
+// }
+
+// $cars = new Volvo("Volvo", "red");
+// $cars->intro();
+
+// abstract classes with argument
+
+abstract class Car{
+    abstract public function name($name);
+}
+
+class Intro extends Car{
+    public function name($name){
+        echo "Car ".$name."<br>";
+    }
+
+}
+
+$cars = new Intro();
+$cars->name("Volvo");
