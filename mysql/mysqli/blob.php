e27a66ef9c2315bbfcfdde3c2e1468c2daae369a
sajib <me.mrsajib@gmail.com>
2022-07-26 12:05:03 +0600

    blob data show problem

--- mysql/mysqli/blob.php
+++ mysql/mysqli/blob.php
@@ -9,9 +9,20 @@ if(mysqli_connect_errno()){
     echo "Success: Connected to database.";
 }
 
-$sql = "insert into images (img) values (?)";
+
+$sql = "select img from images where id=?";
 $stmt = $db->prepare($sql);
-$stmt->bind_param("s", $img);
-$img = file_get_contents("img.jpg");
-$stmt->send_long_data(0, $img);
+$stmt->bind_param("i", $id);
+$id = 1;
 $stmt->execute();
+$stmt->bind_result($image);
+$stmt->fetch();
+header("Content-type:jpg");
+echo '<img src="data:image/jpg;base64,'.base64_encode($image).'"/>';
+
+// $sql = "insert into images (img) values (?)";
+// $stmt = $db->prepare($sql);
+// $stmt->bind_param("s", $img);
+// $img = file_get_contents("img.jpg");
+// $stmt->send_long_data(0, $img);
+// $stmt->execute();
