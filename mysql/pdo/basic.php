10a31fee3faf4e5e73835bd02aefa7a1075862ea
sajib <me.mrsajib@gmail.com>
2022-07-26 12:42:19 +0600

    insert data into databse with pdo prepare statement

--- mysql/pdo/basic.php
+++ mysql/pdo/basic.php
@@ -16,3 +16,15 @@ $result = $db->query($sql);
 foreach ($result as $value) {
     echo $value['name']."<br>";
 }
+
+// insert data 
+$name = "jack";
+$email = "jack@gmail.com";
+$age = 30;
+
+$sql_insert = "insert into my_guests (name, email, age) values (:name, :email, :age)";
+$stmt = $db->prepare($sql_insert);
+$stmt->bindParam(':name', $name);
+$stmt->bindParam(':email', $email);
+$stmt->bindParam(':age', $age);
+$stmt->execute();
