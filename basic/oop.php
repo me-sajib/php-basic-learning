99c600f5667079ed3920f3db12de9dc34f86638d
me-sajib <me.mrsajib@gmail.com>
2022-10-03 15:52:39 -0700

    exploring interface

--- basic/oop.php
+++ basic/oop.php
@@ -1,25 +1,49 @@
 <!-- class object property method -->
 <?php
 
-class Intro{
-    public $name;
-    public $age;
-    const ID = 22;
-    public static $school = "Dhaka College";
+// class Intro{
+//     public $name;
+//     public $age;
+//     const ID = 22;
+//     public static $school = "Dhaka College";
 
-    public function __construct($name, $age){
-        $this->name = $name;
-        $this->age = $age;
-    }
-    public function details(){
-        echo "this person name is ".$this->name;
-    }
+//     public function __construct($name, $age){
+//         $this->name = $name;
+//         $this->age = $age;
+//     }
+//     public function details(){
+//         echo "this person name is ".$this->name;
+//     }
     
-    public static function full_information(){
-        echo "this person age is "." his id: ".Intro::ID;
-        echo "<br> school is ".self::$school;
+//     public static function full_information(){
+//         echo "this person age is "." his id: ".Intro::ID;
+//         echo "<br> school is ".self::$school;
+//     }
+// }
+
+// $result = new Intro("sajib", "23");
+// Intro::full_information();
+
+
+// interface implement
+
+interface School{
+    public function schoolName($name):void;
+    public function address($address);
+}
+
+class Schools implements School{
+    public function schoolName($name):void{
+        echo "school name is ". $name;
+    }
+
+    public function address($address){
+        echo "his school address is ".$address;
     }
 }
 
-$result = new Intro("sajib", "23");
-Intro::full_information();
+
+
+$result = new Schools();
+echo $result->schoolName("rangpur");
+echo $result->address("Rangpur");
